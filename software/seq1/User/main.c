4a8cd1600a40280238a6c45b719e7e76020ecf2a
ldz <2143115598@qq.com>
2025-01-01 18:06:37 +0800

    feat: 添加eeprom保存密码，优化界面

--- software/seq1/User/main.c
+++ software/seq1/User/main.c
@@ -16,6 +16,7 @@ void Oled_Task(void);
 //其他函数
 void oledshow_MainPage(void);
 void oledshow_MasterPage(void);
+void password_clear(void);
 bit str_judge(int *str1, int *str2);
 void str_copy(int *str1, int *str2);
 
@@ -32,7 +33,7 @@ uint8_t Key_Old;
 //任务调度专用变量
 uint16_t data TaskTimer[TASKNUM_MAX];
 TaskStruct data Task[] = {
-    {Led_Task, 100},
+    //{Led_Task, 100},
 	{key_Task, 10},
 	{Oled_Task, 1000}
 };
@@ -40,9 +41,9 @@ TaskStruct data Task[] = {
 char str[20];
 //uint8_t eepromBuf[10];
 int count = 0;
-volatile int password[4] = {0, 0, 0, 0};
-volatile int user_password[4] = {1, 2, 3, 4};
-volatile int master_password[4] = {4, 3, 2, 1};
+volatile char password[4] = "____";
+volatile char user_password[4] = "1234";
+volatile char master_password[4] = "4321";
 
 uint8_t index = 0;
 bit door_flag = 0;
@@ -73,6 +74,13 @@ int main(void)
     //menu_init();
     
     //EEPROM_Write("hello", 0, 5);
+//    EEPROM_Write((uint8_t *)user_password, 0, 4);
+//    delay_ms(20);
+//    EEPROM_Write((uint8_t *)master_password, 8, 4);
+    
+    EEPROM_Read((uint8_t *)user_password, 0, 4);
+    delay_ms(20);
+    EEPROM_Read((uint8_t *)master_password, 8, 4);
     
 	Timer0_Init();
     Uart1_Init();
@@ -95,17 +103,15 @@ void oledshow_MainPage(void)
     sprintf(str, "enter your PIN:");
     OLED_ShowString(0,2,str,8);
     
-    // 清零输入密码
-    password[0] = 0;
-    password[1] = 0;
-    password[2] = 0;
-    password[3] = 0;
-    
-    sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+    // 清空输入密码
+    password_clear();    
+    sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
     OLED_ShowString(0,4,str,8);
     
+    // 门状态
     sprintf(str, "door close");
     OLED_ShowString(0,6,str,8);
+    
 }
 
 void oledshow_MasterPage(void)
@@ -116,17 +122,22 @@ void oledshow_MasterPage(void)
     sprintf(str, "change your PIN:");
     OLED_ShowString(0,2,str,8);
     
-    // 清零输入密码
-    password[0] = 0;
-    password[1] = 0;
-    password[2] = 0;
-    password[3] = 0;
-    
-    sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+    // 清空输入密码
+    password_clear();    
+    sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
     OLED_ShowString(0,4,str,8);
+    
 }
 
-bit str_judge(int *str1, int *str2)
+void password_clear(void)
+{
+    password[0] = '_';
+    password[1] = '_';
+    password[2] = '_';
+    password[3] = '_';
+}
+
+bit str_judge(char *str1, char *str2)
 {
     uint8_t i;
     
@@ -138,7 +149,7 @@ bit str_judge(int *str1, int *str2)
     return 1;
 }
 
-void str_copy(int *str1, int *str2)
+void str_copy(char *str1, char *str2)
 {
     uint8_t i;
     
@@ -207,62 +218,62 @@ void key_Task(void)
 	switch(Key_Down)
 	{
 		case 10:
-            password[index++] = 0;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '0';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
         break;
         
         case 1:
-            password[index++] = 1;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '1';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
         break;
         
         case 2:
-            password[index++] = 2;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '2';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
         break;
         
         case 3:
-            password[index++] = 3;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '3';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
 		break;
         
         case 4:
-            password[index++] = 4;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '4';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
 		break;
         
         case 5:
-            password[index++] = 5;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '5';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
 		break;
         
         case 6:
-            password[index++] = 6;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '6';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
 		break;
         
         case 7:
-            password[index++] = 7;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '7';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
 		break;
         
         case 8:
-            password[index++] = 8;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '8';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
 		break;
         
         case 9:
-            password[index++] = 9;
-            sprintf(str, "     %d  %d  %d  %d", password[0], password[1], password[2], password[3]);
+            password[index++] = '9';
+            sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
             OLED_ShowString(0,4,str,8);
 		break;
         
@@ -280,18 +291,21 @@ void key_Task(void)
             if(Mode == 0)
             {
                 door_flag = str_judge(password, user_password);
+                // 清空输入密码
+                password_clear();
+                sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
+                OLED_ShowString(0,4,str,8);
+                
             }
             // 管理员模式下
             else
             {
                 master_enter_flag = str_judge(password, master_password);
-                // 如果管理员密码输入错误，退回用户模式
-                if(master_enter_flag == 0)
-                {
-                    Mode = 0;
-                    P0 = 0xfe;
-                }
-                    
+                // 清空输入密码
+                password_clear();
+                sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
+                OLED_ShowString(0,4,str,8);
+                
             }
         }
         // 管理员界面
@@ -301,14 +315,27 @@ void key_Task(void)
             if(change_flag == 0)
             {
                 str_copy(user_password, password);
+                
+                EEPROM_Write((uint8_t *)user_password, 0, 4);
+                
+                // 清空输入密码
+                password_clear();
+                sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
+                OLED_ShowString(0,4,str,8);
             }
             // 更改管理员密码
             else
             {
                 str_copy(master_password, password);
+                
+                EEPROM_Write((uint8_t *)master_password, 8, 4);
+                
+                // 清空输入密码
+                password_clear();
+                sprintf(str, "     %c  %c  %c  %c", password[0], password[1], password[2], password[3]);
+                OLED_ShowString(0,4,str,8);
             }
             
-            main_enter_flag = 1;
         }
         
         
@@ -321,9 +348,10 @@ void key_Task(void)
 void Oled_Task(void)
 {
     // 调试代码
-    sprintf(str, "%d%d%d%d-%d%d%d%d", user_password[0], user_password[1], user_password[2], user_password[3],
+    sprintf(str, "%c%c%c%c-%c%c%c%c", user_password[0], user_password[1], user_password[2], user_password[3],
                                       master_password[0], master_password[1], master_password[2], master_password[3]);
     OLED_ShowString(0,7,str,8);
+   
     
     // 是否开门
     if(door_flag)
@@ -331,10 +359,16 @@ void Oled_Task(void)
         sprintf(str, "door open ");
         OLED_ShowString(0,6,str,8);
         
+        delay_ms(1000);
+        door_flag = 0;
+        
         P0 = 0xef;
     }
     else
     {
+        sprintf(str, "door close");
+        OLED_ShowString(0,6,str,8);
+        
         P0 = 0xff;
     }
     
@@ -400,15 +434,6 @@ void Oled_Task(void)
         }
     }
     
-    
-    
-//    
-//    // eeprom测试
-//    EEPROM_Read(eepromBuf, 0, 5);
-//    OLED_ShowString(0,2,(char *)eepromBuf,16);
-       
-//        menu(menuKey_flag);
-//        menuKey_flag = 0;
     
 }
 
